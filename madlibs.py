4aeabf4ebd295f636e356e51d52e3be7361bd99c
Chidubem Nkemka <dubemnkemka@gmail.com>
2024-02-05 21:11:04 +0100

    first commit

--- madlibs.py
+++ madlibs.py
@@ -6,4 +6,8 @@ famous_person = input("Famous person: ")
 madlibs = f"Computer programming is so {adj}! It makes me so excited all the time because \
 I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
+madlib = ("Computer programming is a/an " + adj + " skillset! I can create fantastic apps and " + verb1 +
+" . I could also " + verb2 + " with " + famous_person + " in my free time")
+
 print(madlibs)
+print(madlib)
